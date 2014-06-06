4f71d37fe2825a04d204d8e7c082e94c928ae4aa
tishan89 <dtishanpubudu@gmail.com>
2014-06-06 15:53:41 +0530

    adding SiddhiHive DS and value holder

---
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/internal/ds/SiddhiHiveDS.java
@@ -0,0 +1,48 @@
+package org.wso2.carbon.siddhihive.core.internal.ds;
+
+
+import org.apache.log4j.Logger;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.event.stream.manager.core.EventStreamService;
+import org.wso2.carbon.siddhihive.core.SiddhiHiveServiceInterface;
+import org.wso2.carbon.siddhihive.core.internal.SiddhiHiveService;
+
+/**
+ * @scr.component name="siddhiHive.component" immediate="true"
+ * @scr.reference name="eventstream.service"
+ * interface="org.wso2.carbon.event.stream.manager.core.EventStreamService" cardinality="1..1"
+ * policy="dynamic" bind="setEventStreamService" unbind="unsetEventStreamService"
+ */
+public class SiddhiHiveDS {
+    private static final Logger log = Logger.getLogger(SiddhiHiveDS.class);
+
+    /**
+     * Exposing SiddhiHive OSGi service.
+     *
+     * @param context
+     */
+    protected void activate(ComponentContext context) {
+
+        try {
+            SiddhiHiveService siddhiHiveService = new SiddhiHiveService();
+            SiddhiHiveValueHolder.getInstance().registerSiddhiHiveService(siddhiHiveService);
+            context.getBundleContext().registerService(SiddhiHiveServiceInterface.class.getName(),
+                    siddhiHiveService, null);
+            log.info("Successfully deployed the Siddhi-Hive conversion Service");
+        } catch (Throwable e) {
+            log.error("Can not create the Siddhi-Hive conversion Service ", e);
+        }
+    }
+
+    protected void deactivate(ComponentContext context) {
+        // context.getBundleContext().ungetService();
+    }
+
+    protected void setEventStreamService(EventStreamService eventStreamService) {
+        SiddhiHiveValueHolder.getInstance().setEventStreamService(eventStreamService);
+    }
+
+    protected void unsetEventStreamService(EventStreamService eventStreamService) {
+        SiddhiHiveValueHolder.getInstance().unsetEventStreamService();
+    }
+}
---
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/internal/ds/SiddhiHiveValueHolder.java
@@ -0,0 +1,42 @@
+package org.wso2.carbon.siddhihive.core.internal.ds;
+
+
+import org.wso2.carbon.event.stream.manager.core.EventStreamService;
+import org.wso2.carbon.siddhihive.core.internal.SiddhiHiveService;
+
+public class SiddhiHiveValueHolder {
+    private EventStreamService eventStreamService;
+    private static SiddhiHiveValueHolder siddhiHiveValueHolder;
+    private SiddhiHiveService siddhiHiveService;
+
+    private SiddhiHiveValueHolder() {
+        //Do nothing
+    }
+
+    public static SiddhiHiveValueHolder getInstance() {
+        if (siddhiHiveValueHolder == null) {
+            siddhiHiveValueHolder = new SiddhiHiveValueHolder();
+        }
+        return siddhiHiveValueHolder;
+    }
+
+    public EventStreamService getEventStreamService() {
+        return eventStreamService;
+    }
+
+    public void setEventStreamService(EventStreamService eventStreamService) {
+        this.eventStreamService = eventStreamService;
+    }
+
+    public void unsetEventStreamService() {
+        this.eventStreamService = null;
+    }
+
+    public void registerSiddhiHiveService(SiddhiHiveService siddhiHiveService) {
+        this.siddhiHiveService = siddhiHiveService;
+    }
+
+    public SiddhiHiveService getSiddhiHiveService() {
+        return siddhiHiveService;
+    }
+}
