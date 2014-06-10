a9ad68d43c300cd87e18d302eba95f7337320df9
tishan89 <dtishanpubudu@gmail.com>
2014-06-10 18:01:12 +0530

    adding time window implementation

---
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/headerprocessor/TimeWindowStreamHandler.java
@@ -0,0 +1,55 @@
+package org.wso2.carbon.siddhihive.core.headerprocessor;
+
+
+import org.wso2.carbon.siddhihive.core.configurations.StreamDefinition;
+import org.wso2.carbon.siddhihive.core.utils.Constants;
+import org.wso2.siddhi.query.api.expression.constant.IntConstant;
+import org.wso2.siddhi.query.api.query.input.Stream;
+import org.wso2.siddhi.query.api.query.input.WindowStream;
+import org.wso2.siddhi.query.api.query.input.handler.Window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TimeWindowStreamHandler extends WindowStreamHandler {
+    private String windowIsolatorClause;
+    private String fromClause;
+    private String whereClause;
+    private String schedulingFreq;
+    private WindowStream windowStream;
+    private WindowIsolator windowIsolator;
+    private Map<String, String> result;
+
+    public TimeWindowStreamHandler() {
+        this.windowIsolator = new WindowIsolator();
+    }
+
+    @Override
+    public Map<String, String> process(Stream stream, Map<String, StreamDefinition> streamDefinitions) {
+        this.windowStream = (WindowStream) stream;
+        windowIsolatorClause = generateIsolatorClause(windowStream.getStreamId(), windowStream.getWindow(), streamDefinitions);
+        fromClause = generateFromClause(windowStream.getStreamId());
+        whereClause = generateWhereClause(windowStream.getFilter());
+        schedulingFreq = generateSchedulingFrequency(windowStream.getWindow());
+        result = new HashMap<String, String>();
+        result.put(Constants.FROM_CLAUSE, fromClause);
+        result.put(Constants.WHERE_CLAUSE, whereClause);
+        result.put(Constants.INCREMENTAL_CLAUSE, windowIsolatorClause);
+        result.put(Constants.TIME_WINDOW_FREQUENCY, schedulingFreq);
+        return result;
+    }
+
+    private String generateSchedulingFrequency(Window window) {
+        if (window.getName().equals(Constants.TIME_WINDOW)) {
+            return Constants.DEFAULT_SLIDING_FREQUENCY;
+        } else if (window.getName().equals(Constants.TIME_BATCH_WINDOW)) {
+            return String.valueOf(((IntConstant) window.getParameters()[0]).getValue());
+        }
+        return null;
+    }
+
+    private String generateIsolatorClause(String streamId, Window window, Map<String, StreamDefinition> streamDefinitions) {
+        StreamDefinition streamDefinition = streamDefinitions.get(streamId);
+        return windowIsolator.process(windowStream.getWindow(), streamDefinition);
+    }
+}
--- siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/headerprocessor/WindowStreamHandler.java
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/headerprocessor/WindowStreamHandler.java
@@ -5,40 +5,25 @@ import org.wso2.carbon.siddhihive.core.configurations.StreamDefinition;
 import org.wso2.carbon.siddhihive.core.utils.Constants;
 import org.wso2.siddhi.query.api.query.input.Stream;
 import org.wso2.siddhi.query.api.query.input.WindowStream;
-import org.wso2.siddhi.query.api.query.input.handler.Window;
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class WindowStreamHandler extends BasicStreamHandler {
 
-    private String windowIsolatorClause;
-    private String fromClause;
-    private String whereClause;
     private WindowStream windowStream;
-    private WindowIsolator windowIsolator;
-    private Map<String, String> result;
-
-    public WindowStreamHandler() {
-        this.windowIsolator = new WindowIsolator();
-    }
+    private Object type;
 
     @Override
     public Map<String, String> process(Stream stream, Map<String, StreamDefinition> streamDefinitions) {
-        this.windowStream = (WindowStream) stream;
-        windowIsolatorClause = generateIsolatorClause(windowStream.getStreamId(), windowStream.getWindow(), streamDefinitions);
-        fromClause = generateFromClause(windowStream.getStreamId());
-        whereClause = generateWhereClause(windowStream.getFilter());
-        result = new HashMap<String, String>();
-        result.put(Constants.FROM_CLAUSE, fromClause);
-        result.put(Constants.WHERE_CLAUSE, whereClause);
-        result.put(Constants.INCREMENTAL_CLAUSE, windowIsolatorClause);
-        return result;
+        windowStream = (WindowStream) stream;
+        this.type = windowStream.getWindow().getName();
+        if (type.equals(Constants.TIME_WINDOW) || type.equals(Constants.TIME_BATCH_WINDOW)) {
+            TimeWindowStreamHandler timeWindowStreamHandler = new TimeWindowStreamHandler();
+            return timeWindowStreamHandler.process(windowStream, streamDefinitions);
+        } else if (type.equals(Constants.LENGTH_WINDOW) || type.equals(Constants.LENGTH_BATCH_WINDOW)) {
+            return null; //TODO
+        }
+        return null;
     }
 
-    private String generateIsolatorClause(String streamId, Window window, Map<String, StreamDefinition> streamDefinitions) {
-        StreamDefinition streamDefinition = streamDefinitions.get(streamId);
-        return windowIsolator.process(windowStream.getWindow(), streamDefinition);
-    }
 
 }
