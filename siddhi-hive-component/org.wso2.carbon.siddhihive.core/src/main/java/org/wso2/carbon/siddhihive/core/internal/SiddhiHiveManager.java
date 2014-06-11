403a215394d02926cd59415763467aef7227304e
prasadfernando <bppf16@gmail.com>
2014-06-11 09:52:32 +0530

    table create query

--- siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/internal/SiddhiHiveManager.java
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/internal/SiddhiHiveManager.java
@@ -80,10 +80,13 @@ public class SiddhiHiveManager {
         OutStream outStream = query.getOutputStream();
         StreamDefinitionExt outStreamDefinition = getStreamDefinition(outStream.getStreamId());
 
-        CSVTableCreator CSVTableCreator = new CSVTableCreator();
-        //CSVTableCreator.setQuery(outStreamDefinition);
 
-        String outputQuery = CSVTableCreator.getInsertQuery();
+
+        CSVTableCreator CSVTableCreator = new CSVTableCreator();
+        CSVTableCreator.setQuery(outStreamDefinition);
+
+        String insertQuery = CSVTableCreator.getInsertQuery();
+        String createQuery = CSVTableCreator.getQuery();
         //hiveQuery = outputQuery + "\n" +
 
 
@@ -114,7 +117,7 @@ public class SiddhiHiveManager {
             incrementalClause = " ";
 
        // hiveQuery = outputQuery + "\n" + incrementalClause + "\n" + fromClause + "\n " + selectQuery + "\n " + groupByQuery + "\n " + havingQuery + "\n " + whereClause + "\n ";
-        hiveQuery = outputQuery + "\n" + incrementalClause + "\n" + selectQuery + "\n " + fromClause + "\n " +whereClause + "\n " + groupByQuery + "\n " + havingQuery + "\n ";
+        hiveQuery = createQuery +"\n" +insertQuery + "\n" + incrementalClause + "\n" + selectQuery + "\n " + fromClause + "\n " +whereClause + "\n " + groupByQuery + "\n " + havingQuery + "\n ";
 
         return hiveQuery;
 
