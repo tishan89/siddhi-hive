b485539026ba355e5d4db99b81bcf2428d43160b
tishan89 <dtishanpubudu@gmail.com>
2014-06-21 01:58:07 +0530

    adding zipping utils

---
+++ siddhi-hive-component/org.wso2.carbon.siddhihive.core/src/main/java/org/wso2/carbon/siddhihive/core/utils/ZipppingUtil.java
@@ -0,0 +1,79 @@
+package org.wso2.carbon.siddhihive.core.utils;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipppingUtil {
+
+    List<String> fileList;
+    private String outputZipFile;
+    private String sourceFolder;
+
+    public ZipppingUtil() {
+        fileList = new ArrayList<String>();
+    }
+
+    public void zip(String zipFile, String sourceFile) {
+        sourceFolder = sourceFile;
+        outputZipFile = zipFile;
+        byte[] buffer = new byte[1024];
+
+        try {
+
+            FileOutputStream fos = new FileOutputStream(zipFile);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            System.out.println("Output to Zip : " + zipFile);
+
+            for (String file : this.fileList) {
+
+                ZipEntry ze = new ZipEntry(file);
+                zos.putNextEntry(ze);
+
+                FileInputStream in =
+                        new FileInputStream(sourceFile + File.separator + file);
+
+                int len;
+                while ((len = in.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+
+                in.close();
+            }
+
+            zos.closeEntry();
+            zos.close();
+
+            System.out.println("Done");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void generateFileList(File node) {
+
+        if (node.isFile()) {
+            fileList.add(generateZipEntry(node.getAbsoluteFile().toString()));
+        }
+
+        if (node.isDirectory()) {
+            String[] subNote = node.list();
+            for (String filename : subNote) {
+                generateFileList(new File(node, filename));
+            }
+        }
+
+    }
+
+    private String generateZipEntry(String file) {
+        return file.substring(sourceFolder.length() + 1, file.length());
+    }
+
+}
